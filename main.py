f8ea77a6cbaa1c4801203481d043767f71feb050
Mikhail Beshkarev <krivoship@bk.ru>
2022-02-08 18:02:12 +0300

    Вынес подсчет среднего в отдельную функцию.

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ def get_salaries(vacancies, key, salary_func):
     return salaries
 
 
+def get_average(lower_limit, upper_limit):
+    if not lower_limit or lower_limit == 0:
+        return 0.8*upper_limit
+    elif not upper_limit or upper_limit == 0:
+        return 1.2*lower_limit
+    else:
+        return (lower_limit + upper_limit) / 2
+
+
 def predict_rub_salary_hh(vacancy):
     if not vacancy['salary'] or vacancy['salary']['currency'] != 'RUR':
         return None
@@ -27,27 +36,18 @@ def predict_rub_salary_hh(vacancy):
         lower_limit = vacancy['salary'].get('from', None)
         upper_limit = vacancy['salary'].get('to', None)
 
-        if not lower_limit:
-            lower_limit = 0.6*upper_limit
-        if not upper_limit:
-            upper_limit = 1.4*lower_limit
-
         if vacancy['salary']['gross']:
-            return 0.87*(lower_limit+upper_limit)/2
+            return 0.87*get_average(lower_limit, upper_limit)
         else:
-            return (lower_limit + upper_limit) / 2
+            return get_average(lower_limit, upper_limit)
 
 
 def predict_rub_salary_sj(vacancy):
     if (vacancy['payment_from'] == 0 and vacancy['payment_to'] == 0) or \
             vacancy['currency'] != 'rub':
         return None
-    elif vacancy['payment_from'] == 0:
-        return 0.8*vacancy['payment_to']
-    elif vacancy['payment_to'] == 0:
-        return 1.2*vacancy['payment_from']
     else:
-        return (vacancy['payment_from'] + vacancy['payment_to'])/2
+        return get_average(vacancy['payment_from'], vacancy['payment_to'])
 
 
 def get_hh_statistic(url_hh, language):
